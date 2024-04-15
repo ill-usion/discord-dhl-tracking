ae0696e8d76e3b0a8662d8bb2e1d6068a68c5488
ill-usion <aalhadrami7@gmail.com>
2024-04-15 10:45:02 +0400

    cron job test 2

--- index.ts
+++ index.ts
@@ -1,16 +1,27 @@
+import * as cron from "node-cron";
+
 const webhook = process.env.WEBHOOK_URL || "";
 
-const resp = await fetch(webhook, {
-	method: "POST",
-	body: JSON.stringify({ content: "cron job" }),
-	headers: {
-		"Content-Type": "application/json",
+Bun.serve({
+	fetch(req) {
+		return new Response("OK");
 	},
 });
 
-const status = resp.status;
-console.log(status);
+cron.schedule("*/6 * * * *", async () => {
+	console.log("running a task every 6 minutes");
+	const resp = await fetch(webhook, {
+		method: "POST",
+		body: JSON.stringify({ content: "cron job" }),
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
 
-if (status >= 300) {
-	console.log(await resp.text());
-}
+	const status = resp.status;
+	console.log(status);
+
+	if (status >= 300) {
+		console.log(await resp.text());
+	}
+});
